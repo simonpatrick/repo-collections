58e2e40cdabe6b888f83f96cd4e40eccb7a6a6c0
patrick <patrick@patrickdeMacBook-Pro.local>
2016-01-20 18:52:07 +0800

    add new repo

--- assets/js/repo.js
+++ assets/js/repo.js
@@ -14,18 +14,18 @@ Vue.component('demo-grid',{
     data: function(){
         var sortOrders ={};
         this.columns.forEach(function(key){
-           sortOrders[key]=1
+           sortOrders[key]=1;
         });
         return {
             sortKey: '',
             sortOrders: sortOrders
-        }
+        };
     },
     methods:{
         sortBy:function(key){
-            console.log(key)
+            console.log(key);
             this.sortKey=key;
-            console.log(this.sortOrders[key])
+            console.log(this.sortOrders[key]);
             this.sortOrders[key]=this.sortOrders[key]*-1;
         }
     }
@@ -37,26 +37,26 @@ var coderepos= new Vue({
        searchQuery: '',
        columns: ['location','author','category','language','description'],
        repos: [
-           { author: 'hustcc', category: 'devops' ,language:'PYTHON',location:'https://github.com/hustcc/redis-monitor.git'
-               ,description:'a repo for monitoring redis server'},
-           { author: 'chihongze', category: 'devops' ,language:'PYTHON',location:'https://github.com/chihongze/shard.py.git'
-               ,description:'sharding python operation samples'},
-           { author: 'daikeren', category: 'collections' ,language:'PYTHON,JS',location:'https://github.com/daikeren'
-               ,description:'python,javascript learning repositories'},
-           { author: 'daikeren', category: 'collections' ,language:'PYTHON,JS',location:'https://github.com/binux/pyspider.git'
-               ,description:'python learning repositories'},
-           { author: 'binux', category: 'web crawler' ,language:'PYTHON',location:'https://github.com/binux/pyspider.git'
-               ,description:'a web crawler repository'},
-           { author: 'Panblack', category: 'devops' ,language:'SHELL',location:'https://github.com/Panblack/ezdpl.git'
-               ,description:'shell script for deployment'},
-           { author: 'Shougo', category: 'devops' ,language:'VIM',location:'https://github.com/Shougo/unite.vim'
-               ,description:'The unite or unite.vim plug-in can search and display information'},
-           { author: 'Runscope', category: 'devops' ,language:'PYTHON',location:'https://github.com/Runscope/httpbin'
-               ,description:'HTTP Request & Response Service, written in Python + Flask'
-           },{ author: 'code4craft', category: 'devops' ,language:'JAVA',location:'https://github.com/code4craft/blackhole'
-               ,description:'BlackHole是一个Java编写的DNS服务器'
-           },{ author: 'spotify', category: 'tuning' ,language:'Javascript',location:'https://github.com/spotify/threaddump-analyzer.git'
-               ,description:'Thread Dump Analyzer in a simple website'
+           { author: 'hustcc', category: 'devops' ,language:'PYTHON',
+           location:'https://github.com/hustcc/redis-monitor.git',description:'a repo for monitoring redis server'},
+           { author: 'chihongze', category: 'devops' ,language:'PYTHON',
+           location:'https://github.com/chihongze/shard.py.git',description:'sharding python operation samples'},
+           { author: 'daikeren', category: 'collections' ,language:'PYTHON,JS',location:'https://github.com/daikeren',
+              description:'python,javascript learning repositories'},
+           { author: 'daikeren', category: 'collections' ,language:'PYTHON,JS',location:'https://github.com/binux/pyspider.git',
+               description:'python learning repositories'},
+           { author: 'binux', category: 'web crawler' ,language:'PYTHON',location:'https://github.com/binux/pyspider.git',
+               description:'a web crawler repository'},
+           { author: 'Panblack', category: 'devops' ,language:'SHELL',location:'https://github.com/Panblack/ezdpl.git',
+               description:'shell script for deployment'},
+           { author: 'Shougo', category: 'devops' ,language:'VIM',location:'https://github.com/Shougo/unite.vim',
+               description:'The unite or unite.vim plug-in can search and display information'},
+           { author: 'Runscope', category: 'devops' ,language:'PYTHON',location:'https://github.com/Runscope/httpbin',
+               description:'HTTP Request & Response Service, written in Python + Flask'
+           },{ author: 'code4craft', category: 'devops' ,language:'JAVA',location:'https://github.com/code4craft/blackhole',
+               description:'BlackHole是一个Java编写的DNS服务器'
+           },{ author: 'spotify', category: 'tuning' ,language:'Javascript',location:'https://github.com/spotify/threaddump-analyzer.git',
+               description:'Thread Dump Analyzer in a simple website'
            },{
                author: 'typicode',
                category: 'mockserver',
@@ -177,7 +177,14 @@ var coderepos= new Vue({
              language: 'Javascript',
              location: 'https://github.com/leanote/leanote.git',
              description: '开源记事本'
+           },{
+             author: 'erchu',
+             category: 'Tools',
+             language: 'JAVA',
+             location: 'https://github.com/erchu/bean-cp.git',
+             description: 'Bean Copy tools'
            }
+
        ]
    }
 });
